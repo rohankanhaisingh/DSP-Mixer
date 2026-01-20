3ce6e5443843041ee625daaa9c80aacd4eb6f581
BABAH GEE <61418505+babahgee@users.noreply.github.com>
2026-01-20 15:42:44 +0100

    Window fix

--- src/components/window/prebuilt/SoftClipWindow.tsx
+++ src/components/window/prebuilt/SoftClipWindow.tsx
@@ -9,22 +9,31 @@ export interface SoftClipWindowProperties {
 
 export default function SoftClipWindow({ softClip }: SoftClipWindowProperties) {
 
-    const [initialDrive, setInitialDrive] = useState(softClip.drive);
-    const [drive, setDrive] = useState(initialDrive);
+    const [initialDrive, setInitialDrive] = useState<number>(softClip.drive);
+    const [initialGain, setInitialGain] = useState<number>(softClip.gain);
+
+    const [drive, setDrive] = useState<number>(initialDrive);
+    const [gain, setGain] = useState<number>(initialGain);
 
     useEffect(() => {
         setInitialDrive(softClip.drive);
+        setInitialGain(softClip.gain);
     }, [softClip]);
 
     useEffect(function () {
         softClip.SetDrive(drive);
-    }, [drive]);
+        softClip.SetGain(gain);
+    }, [drive, gain]);
 
     return (
         <div className="grid grid-cols-4 gap-4">
             <div className="flex flex-col gap-[10px] items-center">
                 <p>Drive: {drive.toFixed(1)}</p>
                 <Knob defaultValue={0} value={drive} min={0} max={10} step={0.1} onChange={_drive => setDrive(_drive)} />
+            </div>
+            <div className="flex flex-col gap-[10px] items-center">
+                <p>Gain: {gain.toFixed(1)}</p>
+                <Knob defaultValue={1} value={gain} min={0} max={10} step={0.01} onChange={_gain => setGain(_gain)} />
             </div>
         </div>
     )
